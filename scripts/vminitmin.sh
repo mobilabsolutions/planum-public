df498dd0058b50751c73d35e8d91621ad4e23e3b
Ramin Abdollahi <ramin@mobilabsolutions.com>
2023-12-12 17:30:34 +0100

    vminit min

---
+++ scripts/vminitmin.sh
@@ -0,0 +1,11 @@
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common jq
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install -y docker-ce
+sudo useradd -d /home/planum planum
+sudo usermod -aG docker planum
+sudo su - planum
+
+echo "done"
